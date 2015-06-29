314b30e752303c31294b8152247f7cd3b4139639
shaneshifflett <shifflett.shane@gmail.com>
2015-06-29 18:03:07 -0400

    load all data

--- foiamachine/apps/core/management/commands/load_all_data.py
+++ foiamachine/apps/core/management/commands/load_all_data.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from apps.contacts.utils import create_test_contacts, update_federal_contacts, update_california_contacts
 from apps.requests.utils import populate_record_types
-from apps.government.utils import load_statutes
+from apps.government.utils import load_statutes, load_states
 
 
 class Command(BaseCommand):
@@ -12,3 +12,4 @@ class Command(BaseCommand):
         create_test_contacts()
         populate_record_types()
         load_statutes()
+        load_states()
